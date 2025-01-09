1240c0226e27b59936f7e925fd11492e2f97457d
Yassine-Chalati <yassine.chalati.07@gmail.com>
2025-01-09 09:48:16 +0100

    create vehicle

--- 1-Seek/vehicle.js
+++ 1-Seek/vehicle.js
@@ -1,125 +1,54 @@
 class Vehicle {
   constructor(x, y) {
     // position du véhicule
-    this.pos = createVector(x, y);
+    this.position = createVector(x, y);
     // vitesse du véhicule
-    this.vel = createVector(0, 0);
+    this.velocity = createVector(0, 0);
     // accélération du véhicule
-    this.acc = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
     // vitesse maximale du véhicule
     this.maxSpeed = 6;
     // force maximale appliquée au véhicule
     this.maxForce = 0.25;
     // rayon du véhicule
-    this.r = 16;
-  }
-
-  applyBehaviors(target) {
-    //let force = this.seek(target);
-    let force = this.seek(target);
-    this.applyForce(force);
-  }
-  // seek est une méthode qui permet de faire se rapprocher le véhicule de la cible passée en paramètre
-  seek(target) {
-    // on calcule la direction vers la cible
-    // C'est l'ETAPE 1 (action : se diriger vers une cible)
-    let vitesseSouhaitee = p5.Vector.sub(target, this.pos);
-
-    // Dessous c'est l'ETAPE 2 : le pilotage (comment on se dirige vers la cible)
-    // on limite ce vecteur à la longueur maxSpeed
-    vitesseSouhaitee.setMag(this.maxSpeed);
-
-    // on calcule maintenant force = desiredSpeed - currentSpeed
-    let force = p5.Vector.sub(vitesseSouhaitee, this.vel);
-
-    // et on limite cette force à maxForce
-    force.limit(this.maxForce);
-    return force;
-  }
-
-  flee(target) {
-    // inverse de seek !  RENVOYER UNE FORCE INVERSE A CELLE RENVOYEE
-    // PAR LE COMPORTEMENT SEEK
-  }
-
-  // applyForce est une méthode qui permet d'appliquer une force au véhicule
-  // en fait on additionne le vecteurr force au vecteur accélération
-  applyForce(force) {
-    this.acc.add(force);
+    this.radius = 16;
   }
 
   update() {
-    // on ajoute l'accélération à la vitesse. L'accélération est un incrément de vitesse
-    // (accélératiion = dérivée de la vitesse)
-    this.vel.add(this.acc);
-    // on contraint la vitesse à la valeur maxSpeed
-    this.vel.limit(this.maxSpeed);
-    // on ajoute la vitesse à la position. La vitesse est un incrément de position, 
-    // (la vitesse est la dérivée de la position)
-    this.pos.add(this.vel);
-
-    // on remet l'accélération à zéro
-    this.acc.set(0, 0);
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
   }
 
-  // On dessine le véhicule
   show() {
-    // formes fil de fer en blanc
+    push();
+    fill(255);
     stroke(255);
-    // épaisseur du trait = 2
     strokeWeight(2);
-
-    // formes pleines en blanc
-    fill("blue");
-
-    // sauvegarde du contexte graphique (couleur pleine, fil de fer, épaisseur du trait, 
-    // position et rotation du repère de référence)
-    push();
-    
-    // on déplace le repère de référence.
-    translate(this.pos.x, this.pos.y);
-    // et on le tourne. heading() renvoie l'angle du vecteur vitesse (c'est l'angle du véhicule)
-    rotate(this.vel.heading());
-
-    //circle(0, 0, this.r * 2);
-
-    // Dessin d'un véhicule sous la forme d'un triangle. Comme s'il était droit, avec le 0, 0 en haut à gauche
-    triangle(-this.r, -this.r / 2, -this.r, this.r / 2, this.r, 0);
-    // Que fait cette ligne ?
-    //this.edges();
-    
-    // draw velocity vector
-    pop();
-    this.drawVelocityVector();
-  }
-
-  drawVelocityVector() {
-    push();
-    // Dessin du vecteur vitesse
-    // Il part du centre du véhicule et va dans la direction du vecteur vitesse
-    strokeWeight(3);
-    stroke("red");
-    line(this.pos.x, this.pos.y, this.pos.x + this.vel.x * 10, this.pos.y + this.vel.y * 10);
-    // dessine une petite fleche au bout du vecteur vitesse
-    let arrowSize = 5;
-    translate(this.pos.x + this.vel.x * 10, this.pos.y + this.vel.y * 10);
-    rotate(this.vel.heading());
-    translate(-arrowSize / 2, 0);
-    triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+    translate(this.position.x, this.position.y);
+    rotate(this.velocity.heading());
+    triangle(-this.radius, -this.radius, -this.radius, this.radius, this.radius, 0);
+    fill(255, 0, 0);
+    stroke(255, 255, 255);
+    triangle(-this.radius / 2, -this.radius / 2, -this.radius / 2, this.radius / 2, this.radius / 2, 0);
+    fill(255, 0, 0);
+    square(-this.radius - 7, -this.radius / 2 - 3, 7);
+    square(-this.radius - 7, this.radius / 2 - 4, 7);
     pop();
   }
-  
-  // que fait cette méthode ?
-  edges() {
-    if (this.pos.x > width + this.r) {
-      this.pos.x = -this.r;
-    } else if (this.pos.x < -this.r) {
-      this.pos.x = width + this.r;
-    }
-    if (this.pos.y > height + this.r) {
-      this.pos.y = -this.r;
-    } else if (this.pos.y < -this.r) {
-      this.pos.y = height + this.r;
-    }
+
+  seek(target) {
+    let desiredVelocity = p5.Vector.sub(target, this.position);
+    desiredVelocity.limit(this.maxSpeed);
+    let steeringForce = p5.Vector.sub(desiredVelocity, this.velocity);
+    steeringForce.limit(this.maxForce);
+    this.applyForce(steeringForce);
   }
+
+  applyForce(force) {
+    this.acceleration.add(force);
+  }
+
+
 }
